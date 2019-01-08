75757f0666aac3866eca214ae3d2174ac7a03ba0
open-trade <info@opentradesolutions.com>
2019-01-08 14:16:22 +0800

    use Visit in sample.cc

--- src/alpha/sample/sample.cc
+++ src/alpha/sample/sample.cc
@@ -8,8 +8,18 @@ struct Sample : public Alpha {
     di = di - delay();
     for (auto ii = 0; ii < num_instruments(); ++ii) {
       if (!valid(di, ii)) continue;
-      alpha[ii] = -(close_price.Value<double>(di, ii) -
-                    close_price.Value<double>(di - 2, ii));
+      double px_2;
+      double px_0;
+      close_price.Visit<double>(di, ii, -2, [&](double px, int offset) {
+        if (offset == -2)
+          px_2 = px;
+        else if (!offset)
+          px_0 = px;
+        return false;
+      });
+      alpha[ii] = -(px_0 - px_2);
+      // alpha[ii] = -(close_price.Value<double>(di, ii) -
+      //            close_price.Value<double>(di - 2, ii));
     }
   }
   ArrowTable close_price;
